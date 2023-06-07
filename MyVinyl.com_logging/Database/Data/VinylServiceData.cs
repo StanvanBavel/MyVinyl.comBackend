1e773cd81c930334624927c2d1bbb70c3d62017e
StanvanBavel <73948369+StanvanBavel@users.noreply.github.com>
2023-06-07 12:12:40 +0200

    added Logging

---
+++ MyVinyl.com_logging/Database/Data/VinylServiceData.cs
@@ -0,0 +1,11 @@
+﻿namespace MyVinyl.com_logging.Database.Data
+{
+    public class VinylServiceData
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
---
+++ MyVinyl.com_logging/Services/LoggingService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyVinyl.com_logging.Database.Contexts;
+using MyVinyl.com_logging.Database.Data;
+
+namespace MyVinyl.com_logging.Services
+{
+    public class LoggingService
+    {
+        private readonly LoggingDbContext _dbContext;
+
+        public LoggingService(IConfiguration configuration)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<LoggingDbContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("ConnectionString"));
+
+            _dbContext = new LoggingDbContext(optionsBuilder.Options);
+        }
+
+        public void VinylServiceDataToDatabase(VinylServiceData vinylServiceData)
+        {
+            // Create a new LogEntry object to store in the database
+            var logEntry = new VinylServiceData
+            {
+                Id = vinylServiceData.Id,
+                Name = vinylServiceData.Name,
+                Description = vinylServiceData.Description,
+                Image = vinylServiceData.Image,
+                IsActive = vinylServiceData.IsActive
+            };
+
+            // Add the LogEntry to the database and save changes
+            _dbContext.VinylServiceEntries.Add(logEntry);
+            _dbContext.SaveChanges();
+        }
+    }
+}
